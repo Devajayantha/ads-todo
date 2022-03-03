8e1d470f9aa2177b6290e6a4de1b0d2c2815599e
DevaJayantha <devajayantha888@gmail.com>
2022-03-03 13:23:05 +0800

    add column in todo migration

--- database/migrations/1646236961445_todos.ts
+++ database/migrations/1646236961445_todos.ts
@@ -6,6 +6,7 @@ export default class Todos extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('name', 100)
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
